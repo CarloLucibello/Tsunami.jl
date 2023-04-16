238783f7bd16b5faa06753ac01bf23ab9cb737f5
CarloLucibello <carlo.lucibello@gmail.com>
2023-04-16 12:21:05 +0200

    cleanup

--- test/fluxmodule.jl
+++ test/fluxmodule.jl
@@ -10,9 +10,8 @@ end
         struct ParModule{A,B} <: FluxModule; a::A; b::B; end
         m = ParModule(rand(2), rand(2))
         m2 = m |> Flux.f32 
-        @test m2.net isa AbstractArray{Float32}
+        @test m2.a isa Vector{Float32}
     end
-
 end
 
 @testset "check_fluxmodule" begin
