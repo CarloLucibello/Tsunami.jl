48540f001c48fa54390d8e98d623fdd44ff4607d
CarloLucibello <carlo.lucibello@gmail.com>
2023-04-16 11:33:12 +0200

    add test

--- test/fluxmodule.jl
+++ test/fluxmodule.jl
@@ -5,6 +5,14 @@ end
 @testset "functor" begin
     m = TestModule1()
     @test Functors.children(m) == (; net = m.net, tuple_field = m.tuple_field)
+
+    @testset "parametric modules" begin
+        struct ParModule{A,B} <: FluxModule; a::A; b::B; end
+        m = ParModule(rand(2), rand(2))
+        m2 = m |> Flux.f32 
+        @test m2.net isa AbstractArray{Float32}
+    end
+
 end
 
 @testset "check_fluxmodule" begin
