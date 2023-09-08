03bf40c8c943e100ba09ad53e7d1b0931419ae80
Carlo Lucibello <carlo.lucibello@gmail.com>
2023-09-08 07:10:17 +0200

    use Flux.get_device (#61)

--- src/foil.jl
+++ src/foil.jl
@@ -55,101 +55,40 @@ function Foil(;
     return Foil(device, fprec, precision) 
 end
 
+const accelerator_symbol_to_string = Dict(
+    :cpu => "CPU",
+    :cuda => "CUDA",
+    :amdgpu => "AMDGPU",
+    :amd => "AMDGPU",
+    :metal => "Metal"
+)
+
 function select_device(accelerator::Symbol, idx_devices)
     if accelerator ∈ (:cpu, :cuda, :amdgpu, :amd, :metal)
-        return get_device(accelerator, idx_devices, strict=true)
+        idx = flux_device_idx(idx_devices)
+        device = Flux.get_device(accelerator_symbol_to_string[accelerator], idx)
     elseif accelerator ∈ (:gpu, :auto)
-        device = get_device(:cuda, idx_devices, strict=false)
-        if device === nothing
-            device = get_device(:amdgpu, idx_devices, strict=false)
+        device = Flux.get_device()
+        if device === :gpu && device isa Flux.FluxCPUDevice
+            @error "No GPU device found. Try importing a gpu package (`using CUDA`, `using AMDGPU`, or `using Metal`).
+                    As an alternative, select `:auto` or `:cpu` as accelerator."
         end
-        if device === nothing
-            device = get_device(:metal, idx_devices, strict=false)
-        end
-        
-        if device === nothing
-            if accelerator == :gpu
-                @error "No GPU device found. Try selecting a more specific accelerator (e.g. `:cuda`) or use `:cpu` instead."
-            else # with :auto we allow CPU fallback
-                device = get_device(:cpu, idx_devices, strict=true)
-            end
-        end
-        return device
     else
         throw(ArgumentError("accelerator must be one of :cpu, :gpu, :auto, :cuda, :amdgpu, :metal"))
     end
+    return device
 end
 
-function get_device(accelerator::Symbol, idx_devices; strict=true)
-    if accelerator == :cpu
-        return FoilCPUDevice()
-    elseif accelerator == :cuda
-        device = FoilCUDADevice()
-        if !is_cuda_available()
-            if strict
-                @error """Trying to use the accelerator `:$(accelerator)` but the trigger package $(device.pkgid) is not loaded
-                or is not functional. Please load the package or select a different accelerator."""
-            else
-                return nothing
-            end
-        end
-        if Flux.GPU_BACKEND != "CUDA"
-            Flux.gpu_backend!("CUDA")
-        end
-        select_device!(device, idx_devices)
-        return device
-    
-    elseif accelerator == :amdgpu || accelerator == :amd
-        device = FoilAMDGPUDevice()
-        if !is_amdgpu_available()
-            if strict
-                @error """Trying to use the accelerator `:$(accelerator)` but the trigger package $(device.pkgid) is not loaded
-                    or is not functional. Please load the package or select a different accelerator."""
-            else
-                return nothing
-            end
-        end
-        if Flux.GPU_BACKEND != "AMD"
-            Flux.gpu_backend!("AMD")
-        end
-        select_device!(device, idx_devices)
-        return device
-    elseif accelerator == :metal
-        device = FoilMetalDevice()
-        if !is_metal_available()
-            if strict
-                @error """Trying to use the accelerator `:$(accelerator)` but the trigger package $(device.pkgid) is not loaded
-                    or is not functional. Please load the package or select a different accelerator."""
-            else
-                return nothing
-            end
-        end
-        if Flux.GPU_BACKEND != "Metal"
-            Flux.gpu_backend!("Metal")
-        end
-        select_device!(device, idx_devices)
-        return device
-    else
-        throw(ArgumentError("accelerator must be one of :auto, :cpu, :gpu, :cuda, :metal, :amdgpu"))
-    end
-end
+flux_device_idx(idx_devices::Nothing) = 0
 
-select_device!(device::AbstractFoilDevice, idx_devices::Nothing)  = nothing
-
-function select_device!(device::AbstractFoilDevice, idx_devices::Int)
+function flux_device_idx(idx_devices::Int)
     @assert idx_devices == 1 "Only one device is supported"
+    return 0
 end
 
-
-function select_device!(device::FoilMetalDevice, idx_devices::Union{Vector{Int}, Tuple})
+function flux_device_idx(idx_devices::Union{Vector{Int}, Tuple})
     @assert length(idx_devices) == 1 "Only one device is supported"
-    # noop since cannot have more than one metal device
-end
-
-function select_device!(device::AbstractFoilGPUDevice, idx_devices::Union{Vector{Int}, Tuple})
-    @assert length(idx_devices) == 1 "Only one device is supported"
-    GPUPkg = Base.loaded_modules[device.pkgid]
-    GPUPkg.device!(idx_devices[1])
+    return idx_devices[1]
 end
 
 to_device(foil::Foil) =  foil.device
@@ -157,7 +96,7 @@ to_device(foil::Foil, x) =  x |> foil.device
 to_precision(foil::Foil) = foil.fprec
 to_precision(foil::Foil, x) = x |> foil.fprec
 
-is_using_gpu(foil::Foil) = foil.device isa AbstractFoilGPUDevice
+is_using_gpu(foil::Foil) = !(foil.device isa Flux.FluxCPUDevice)
 
 function setup(foil::Foil, model, optimisers)
     model = model |> to_precision(foil) |> to_device(foil)
