0fb09512c5c4579723286e3952d6c4e61654ca56
Carlo Lucibello <carlo.lucibello@gmail.com>
2025-12-10 15:09:35 +0100

    fix tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -11,12 +11,19 @@ TEST_CPU = get(ENV, "TSUNAMI_TEST_CPU", "true") == "true"
 TEST_GPU = get(ENV, "TSUNAMI_TEST_CUDA", "false") == "true" ||
            get(ENV, "TSUNAMI_TEST_AMDGPU", "false") == "true" ||
            get(ENV, "TSUNAMI_TEST_Metal", "false") == "true"
-                   
+TEST_ENZYME = VERSION < v"1.12" # Enzyme.jl only supports up to Julia 1.11 as of December 2025
+
 if TEST_CPU
-    @run_package_tests filter = ti -> :gpu ∉ ti.tags
+    @run_package_tests filter = ti -> :gpu ∉ ti.tags && :enzyme ∉ ti.tags
 end
 if TEST_GPU
-    @run_package_tests filter = ti -> :gpu ∈ ti.tags
+    @run_package_tests filter = ti -> :gpu ∈ ti.tags && :enzyme ∉ ti.tags
+end
+if TEST_ENZYME
+    @run_package_tests filter = ti -> :enzyme ∈ ti.tags && :gpu ∉ ti.tags
+    if TEST_GPU
+        @run_package_tests filter = ti -> :enzyme ∈ ti.tags && :gpu ∈ ti.tags
+    end
 end
 
 @testset "Examples" begin
